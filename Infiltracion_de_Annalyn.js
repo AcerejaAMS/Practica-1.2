db7fedac62f5b2d0b64c39a6e2cc0f3cad09b0a3
AcerejaAMS <amoraess1412@gmail.com>
2024-03-07 00:08:46 -0600

    Creacion estructura y logica

--- Infiltracion_de_Annalyn.js
+++ Infiltracion_de_Annalyn.js
@@ -1,2 +1,68 @@
 //Infiltracion de Annalyn
-console.log("AAAa")
+
+//Creacion de funciones de apoyo
+function valorIndice(p){
+    v=Math.random(1);
+
+    if(p===1){
+        if(v<=0.5){
+            return(2);
+        }
+        else{
+            return(3);
+        }
+    }else{
+        if(v<=0.5){
+            return(0);
+        }
+        else{
+            return(1);
+        }
+    }
+}
+
+//Define variables y estados
+let estado={0:"Despierto", 1:"Dormido", 2:"Perro", 3:"SinPerro"};
+let caballero=valorIndice(0);
+let arquero=valorIndice(0);
+let prisionero=valorIndice(0);
+let annalyn=valorIndice(1);
+
+console.log("\t\t----------Opciones----------");
+//Para ataque rapido
+if(estado[caballero]=="Dormido"){
+    console.log("\t[1] Ataque Rapido\t(Disponible)");
+}else{
+    console.log("\t[1] Ataque Rapido\t(No disponible)");
+}
+
+//Para espiar
+if(estado[caballero]=="Despierto" || estado[arquero]=="Despierto"){
+    console.log("\t[2] Espia\t(Disponible)");
+}else{
+    console.log("\t[2] Espia\t(No disponible)");
+}
+
+//Para señalar al prisionero
+if(estado[prisionero]=="Despierto" && estado[arquero]=="Dormido"){
+    console.log("\t[3] Señalar al prisionero\t(Disponible)");
+}else{
+    console.log("\t[3] Señalar al prisionero\t(No disponible)");
+}
+
+//Prisionero libre
+console.log("\t[4] Prisionero libre");
+//Con perro
+if(estado[annalyn]=="Perro" && estado[arquero]=="Dormido"){
+    console.log("\t\t1) El perro protege a Annalyn\t(Disponible)");
+}else{
+    console.log("\t\t1) El perro protege a Annalyn\t(No disponible)");
+}
+//Sin perro
+if(estado[annalyn]=="SinPerro" && estado[prisionero]=="Despierto" && estado[arquero]=="Dormido" && estado[caballero]=="Dormido"){
+    console.log("\t\t2) Ser astutos\t(Disponible)");
+}else{
+    console.log("\t\t2) Ser astutos\t(No disponible)");
+}
+
+ 
